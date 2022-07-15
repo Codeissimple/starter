1440a9101105d956fb90c8a71496974f1639b333
Adam <adam.jkirjacev@gmail.com>
2022-07-14 19:59:58 -0700

    func

--- script.js
+++ script.js
@@ -26,3 +26,22 @@ const appleJuice = fruitProcessor(14, 89);
 console.log(appleJuice);
 
 */
+//function declaration
+
+const age3 = calcAge1(1991); 
+console.log('first call than declare');
+
+function calcAge1(birthYear) {
+    return 2022 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+console.log(age1);
+
+//function expression
+const calcAge2 = function(birthYear){
+    return 2022 - birthYear;
+}
+
+const age2 = calcAge1(1991);
+console.log(age1, age2);
