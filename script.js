c5c4c5a74dcb143ef85fff2dbe36bdb9f1c3a26a
Adam <adam.jkirjacev@gmail.com>
2022-07-14 19:52:53 -0700

    d

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 let hasDriverLicense = false;
 const passTest = true;
 
@@ -8,3 +8,21 @@ if (hasDriverLicense) console.log(`I can drive :D`);
 
 const interface = 'Audio';
 const private = 5555;
+
+
+function logger(){
+    console.log('I\'m a function');
+}
+logger();
+
+
+function fruitProcessor(apples, oranges) {
+    
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(14, 89);
+console.log(appleJuice);
+
+*/
